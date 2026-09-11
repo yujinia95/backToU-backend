fa8fc4d8212f45d370b93cc99176146d26c5864f
hoonji choi <hchoi31@student.ubc.ca>
2026-09-11 10:04:58 -0700

    Add database connection setup

---
+++ app/database.py
@@ -0,0 +1,20 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+
+connection = psycopg2.connect(
+    host=db_host,
+    port=db_port,
+    dbname=db_name,
+    user=db_user,
+    password=db_password
+)
+
+print("Connected to the database successfully!")
