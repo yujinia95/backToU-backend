9f86c7e2ccd61360d3e0a279ecec7bedc8b60f35
jade <jade.jinoklee@gmail.com>
2026-09-12 01:03:35 -0700

    Add Argon2 password hashing and update password column

--- schema.sql
+++ schema.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(50) NOT NULL UNIQUE,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    password_hash TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
